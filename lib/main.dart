d2c37e942ee6c726af39904abba03f25ef2591da
Fabiano Pereira <fabianop971@gmail.com>
2025-05-24 10:13:08 -0300

    Fix: resolve error fetch

--- lib/main.dart
+++ lib/main.dart
@@ -213,8 +213,22 @@ class _MealsState extends State<Meals> {
         title: 'Carregando',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 20,
+              fontFamily: "Raleway",
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+          primaryColor: Colors.blue,
+          fontFamily: "Raleway",
           colorScheme: ColorScheme(
-            primary: Colors.blue,
+            primary: Colors.white,
             brightness: Brightness.light,
             onPrimary: Colors.blue,
             secondary: Color.fromRGBO(34, 242, 187, 1),
@@ -223,6 +237,20 @@ class _MealsState extends State<Meals> {
             onError: Colors.red,
             surface: Color.fromRGBO(255, 254, 229, 1),
             onSurface: Color.fromRGBO(255, 254, 229, 1),
+          ),
+          textTheme: ThemeData.light().textTheme.copyWith(
+            titleMedium: TextStyle(
+              fontSize: 20,
+              fontFamily: "Raleway",
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            bodyMedium: TextStyle(
+              fontSize: 18,
+              fontFamily: "RobotoCondensed",
+              fontWeight: FontWeight.w400,
+              color: Colors.black87,
+            ),
           ),
         ),
         home: LoadingScreen(),
--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/sidebar.dart';
 
 class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: CircularProgressIndicator()));
+    return Scaffold(
+      appBar: AppBar(title: Text("Todas as categorias")),
+      body: Center(child: CircularProgressIndicator(color: Colors.blue)),
+      drawer: Sidebar(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        currentIndex: 1,
+        unselectedItemColor: Theme.of(context).colorScheme.primary,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        onTap: (int value) {},
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "Categorias",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favoritos",
+          ),
+        ],
+      ),
+    );
   }
 }
